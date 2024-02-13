3bc42ab829a911a9ec3f7f32d70fdf455ac88285
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-02-13 12:59:17 -0800

    change evnviornmental variables

--- src/Dispatch.jsx
+++ src/Dispatch.jsx
@@ -4,7 +4,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export const loginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const response = await axios.post(SERVER_URL + "/auth/login", user);
+    const response = await axios.post(`${SERVER_URL}/auth/login`, user);
     dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({ type: "LOGIN_ERROR", payload: err });
